ab8a1d1dcf40c13212a9e0d0635a17dc1d041714
Amit-Dagan <amitdagan78@gmail.com>
2025-03-06 17:21:00 +0200

    new

--- django_proj/settings.py
+++ django_proj/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'python_proj',
     'rest_framework',
-    'users'
+    'users',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication ',
+    ]
+}
--- users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login_user', views.login_user),
-    path('signin_user', views.signin_user),
-]
